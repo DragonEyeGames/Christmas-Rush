1bffcbe25d910faa5ebb9a3617ea4c63081b38c3
Taxalottle Syndicate <bthole41@gmail.com>
2025-11-17 15:56:05 -0500

    Light controlling
    There is a light ring around the player and unseen things are not visible!

--- player.gd
+++ player.gd
@@ -2,13 +2,13 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -500.0
 var coyoteTimer=0.0
 var coyoteTime=0.5
 
+var lightColliding=[]
 
 func _physics_process(delta: float) -> void:
-	print(coyoteTimer)
 	# Add the gravity.
 	if not is_on_floor():
 		coyoteTimer+=delta
@@ -29,3 +29,22 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	lightVisibility()
+
+func lightVisibility():
+	for item in lightColliding:
+		$RayCast2D.set_deferred("target_position", to_local(item.global_position))
+		await get_tree().process_frame
+		if($RayCast2D.is_colliding()):
+			item.visible=false
+		else:
+			item.visible=true
+
+func _light_entered(area: Area2D) -> void:
+	lightColliding.append(area.get_parent())
+
+
+func _light_exited(area: Area2D) -> void:
+	if(area.get_parent() in lightColliding):
+		lightColliding.erase(area.get_parent())
+		area.get_parent().visible=false

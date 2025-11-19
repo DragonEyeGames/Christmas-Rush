d39d33980fe2af40e74700b03b595eb1a6b19768
Taxalottle Syndicate <bthole41@gmail.com>
2025-11-19 16:06:20 -0500

    Level Finishing
    You can now finish the level! Just need a screen to tell you that...

--- tree.gd
+++ tree.gd
@@ -1,6 +1,7 @@
 extends AnimatedSprite2D
 
 var placed=false
+var collided:=false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -8,17 +9,21 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	if(Input.is_action_just_pressed("Interact")):
+	if(Input.is_action_just_pressed("Interact") and collided):
 		if(not placed):
 			$Gifts.visible=true
+			$AnimationPlayer.play("unoutline")
 			placed=true
+			GameManager.placed=true
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if(body is Player and not placed):
 		$AnimationPlayer.play("outline")
+		collided=true
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if(body is Player):
 		$AnimationPlayer.play("unoutline")
+		collided=false

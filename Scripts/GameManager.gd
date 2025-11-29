026b5dfb890fde33880611a1dfc99ed1ec34c2bf
Taxalottle Syndicate <bthole41@gmail.com>
2025-11-28 21:14:26 -0500

    Sprite for chimney
    the chimney fireplace thing now has some cool sprites

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -7,7 +7,7 @@ var overlay:CanvasLayer
 var level:=0
 var levelNode:Level
 var camera:Camera2D
-var stars:=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+var stars:=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 var unlockedLevel:=20
 var cameraFollow
 var playerNode
